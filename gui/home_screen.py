0c5cc723f0186f09386427f115568ee1e44a4a3f
dimitrijekaranfilovic <dimitrije.karanfilovic@uns.ac.rs>
2019-12-31 16:56:34 +0100

    Added license.

--- gui/home_screen.py
+++ gui/home_screen.py
@@ -1,3 +1,20 @@
+"""
+    Game of Hangman written in Python.
+    Copyright (C) 2019  Dimitrije Karanfilović
+    This file is part of pso.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from managers.word_manager import WordManager
